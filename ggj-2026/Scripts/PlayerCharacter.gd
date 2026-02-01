76c33a9d9d7b38534670b5ae4e8774c4f47e4649
Jeff Tucker <jtucker@epic.com>
2026-01-31 22:34:33 -0600

    animations

--- ggj-2026/Scripts/PlayerCharacter.gd
+++ ggj-2026/Scripts/PlayerCharacter.gd
@@ -1,12 +1,16 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 200.0
+const JUMP_VELOCITY = -300.0
 
 @export var currentOrb: RigidBody2D
 @onready var aura = $"Aura?"
 @onready var globalOrb = $"../TheORB"
+@onready var playerSprite = $Sprite2D
+@onready var animator = $Sprite2D/AnimationPlayer
+
+var lastAnimation = 0;
 
 signal pickup
 
@@ -22,7 +26,7 @@ func _input(event):
 	return
 
 func _physics_process(delta: float) -> void:
-	#We need to check proximity to the orb
+	#We need to check proximity to the orb and adjust collision appropriately
 	if(global_position.distance_to(globalOrb.global_position) < 75 || currentOrb!=null):
 		self.set_collision_layer_value(4, true)
 		self.set_collision_mask_value(4, true)
@@ -33,6 +37,7 @@ func _physics_process(delta: float) -> void:
 		self.set_collision_mask_value(3, true)
 		self.set_collision_layer_value(4, false)
 		self.set_collision_mask_value(4, false)
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
@@ -48,12 +53,104 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	chooseAnimation(velocity.x,velocity.y)
+	
 	move_and_slide()
 	
+func chooseAnimation(xdir: float,ydir: float):
+	#lastAnimation guide:
+	# idle = 1
+	# orbidle = 2
+	# run = 3
+	# orbrun = 4
+	# jump = 5
+	# fall = 6
+	# jumporb = 7
+	# fallorb = 8
+	# pickup = 9
+	
+	#check for idle
+	if xdir == 0 && is_on_floor() && currentOrb==null && lastAnimation!=1:
+		playerSprite.frame = 0
+		animator.play("idle")
+		lastAnimation = 1
+	#idle with orb
+	elif xdir == 0 && is_on_floor() && currentOrb!=null && lastAnimation!=2:
+		playerSprite.frame = 0
+		animator.play("orbidle")
+		lastAnimation = 2
+	#moving right on ground without orb
+	elif xdir >0 && is_on_floor() && currentOrb==null:
+		playerSprite.flip_h = 0
+		if(lastAnimation!=3):
+			playerSprite.frame = 0
+			animator.play("run")
+			lastAnimation = 3
+	#moving left on ground without orb
+	elif xdir <0 && is_on_floor() && currentOrb==null:
+		playerSprite.flip_h = 1
+		if(lastAnimation!=3):
+			playerSprite.frame = 0
+			animator.play("run")
+			lastAnimation = 3
+	#moving right on ground with orb
+	elif xdir >0 && is_on_floor() && currentOrb!=null:
+		playerSprite.flip_h = 0
+		if(lastAnimation!=4):
+			playerSprite.frame = 0
+			animator.play("orbrun")
+			lastAnimation = 4
+	#moving left on ground with orb
+	elif xdir <0 && is_on_floor() && currentOrb!=null:
+		playerSprite.flip_h = 1
+		if(lastAnimation!=4):
+			playerSprite.frame = 0
+			animator.play("orbrun")
+			lastAnimation = 4
+	#jumping without orb
+	elif ydir <=0 && !is_on_floor() && currentOrb==null:
+		if(xdir > 0):
+			playerSprite.flip_h = 0
+		else:
+			playerSprite.flip_h = 1
+		if(lastAnimation!=5):
+			playerSprite.frame = 0
+			animator.play("jump")
+			lastAnimation = 5
+	#falling without orb
+	elif ydir >0 && !is_on_floor() && currentOrb==null:
+		if(xdir > 0):
+			playerSprite.flip_h = 0
+		else:
+			playerSprite.flip_h = 1
+		if(lastAnimation!=6):
+			playerSprite.frame = 0
+			animator.play("fall")
+			lastAnimation = 6
+	#jump with orb
+	elif ydir <=0 && !is_on_floor() && currentOrb!=null:
+		if(xdir > 0):
+			playerSprite.flip_h = 0
+		else:
+			playerSprite.flip_h = 1
+		if(lastAnimation!=7):
+			playerSprite.frame = 0
+			animator.play("orbjump")
+			lastAnimation = 7
+	#fall with orb
+	elif ydir >0 && !is_on_floor() && currentOrb!=null:
+		if(xdir > 0):
+			playerSprite.flip_h = 0
+		else:
+			playerSprite.flip_h = 1
+		if(lastAnimation!=8):
+			playerSprite.frame = 0
+			animator.play("orbfall")
+			lastAnimation = 8
+		
 func applyOrbCarried(orb: RigidBody2D):
 	currentOrb = orb
-	print("picked up " + currentOrb.to_string())
 	self.set_collision_layer_value(4, true)
 	self.set_collision_mask_value(4, true)
 	self.set_collision_layer_value(3, false)
@@ -65,7 +162,6 @@ func applyOrbCarried(orb: RigidBody2D):
 	return
 	
 func dropTheOrb():
-	print("Dropped " + currentOrb.to_string())
 	currentOrb.position = self.position
 	currentOrb.process_mode = Node.PROCESS_MODE_INHERIT
 	currentOrb.visible = true
